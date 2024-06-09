2fe72d39ecf28da9e44001bf125b9d99c0fb0066
Pete Mason <pete.dev@robot9.uk>
2024-06-09 08:30:14 +0100

    Style calculator; add Key/Mouse handlers

--- calculator.js
+++ calculator.js
@@ -1,1 +1,39 @@
+const validInput = [
+  "AC",
+  "+/-",
+  "%",
+  "/",
+  "*",
+  "-",
+  "+",
+  "=",
+  ".",
+  "9",
+  "8",
+  "7",
+  "6",
+  "5",
+  "4",
+  "3",
+  "2",
+  "1",
+  "0",
+];
+
 const calculator = document.querySelector("#calculator");
+const buttons = document.querySelector("#buttons");
+
+calculator.addEventListener("keyup", handleKeyPress);
+buttons.addEventListener("click", handleButtonClick);
+
+function handleKeyPress(e) {
+  handleInput(e.key);
+}
+function handleButtonClick(e) {
+  handleInput(e.target.textContent);
+}
+function handleInput(input) {
+  if (validInput.includes(input)) {
+    console.log(input);
+  }
+}
